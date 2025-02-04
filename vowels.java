385dd2e80b676cc03a10cf02ccb7eb8175f6ced6
varsithaanand <varshithaanand2007@gmail.com>
2025-02-04 18:11:42 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,17 @@
+class vowels
+{
+public static void main(String[] args)
+{
+char ch='E';
+switch(ch)
+{
+case'A';case'E';case'I';case'O';case'U';
+System.out.println(ch+"is a vowel");
+break;
+default;
+System.out.println(ch+"is not a vowel");
+}
+}
+
+
+
