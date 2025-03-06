200323c5373efff4c23af4575a82ac84116295ca
varsithaanand <varshithaanand2007@gmail.com>
2025-03-06 11:27:09 +0530

    Create BMI.java
    thank

---
+++ BMI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter weight (kg): ");
+        double weight = scanner.nextDouble();
+        
+        System.out.print("Enter height (m): ");
+        double height = scanner.nextDouble();
+        
+        double bmi = weight / (height * height);
+        
+        System.out.println("Your BMI: " + bmi);
+        
+        scanner.close();
+    }
+}
